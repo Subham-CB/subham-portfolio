24e498bf7ae35fd6ac270eafb573cc742c95fd3f
Subham <de.subham1@gmail.com>
2026-04-08 11:35:41 +0200

    Adding mode to vite config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: "/subham-portfolio",
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    base: mode === 'production' ? '/subham-portfolio/' : '/',
+  }
 })
